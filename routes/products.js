55db505353214ab0c92cd20666cfd2d4becc950b
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-02-04 15:43:24 +0100

    add new routes

--- routes/products.js
+++ routes/products.js
@@ -7,11 +7,49 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   Product.find({})
     .then((products) => {
-      res.render('products/products', { products: products });
+      res.render('products/products', { products });
+    })
+    .catch(next);
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('products/new');
+});
+
+router.post('/', (req, res, next) => {
+  const { name, price, imageURL } = req.body;
+  // const name = req.body.name;
+  // const price = req.body.price;
+  // const imageURL = req.body.imageURL;
+  Product.create({ name, price, imageURL })
+    .then((result) => {
+      res.redirect('/products');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Product.findById(id)
+    .then((product) => {
+      res.render('products/detail', { product });
     })
     .catch((error) => {
       next(error);
     });
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Product.findByIdAndDelete(id)
+    .then((product) => {
+      res.redirect('/products');
+    })
+    .catch((error) => {
+      next(error)
+    })
 });
 
 module.exports = router;
